f5c949176ec561af401eed964f163f14a6f1f52f
Myloslava <88142576+MyloslavaM@users.noreply.github.com>
2024-02-06 03:11:37 +0100

    update

--- src/pageObjects/BasePage.js
+++ src/pageObjects/BasePage.js
@@ -1,24 +1,24 @@
 import Header from "../components/Header.js";
-import BaseComponent from "../components/BaseComponent.js";
+import BaseComponent from "../components/BaseComponent.js"
 
 export default class BasePage extends BaseComponent{
-  constructor(page, url, waitPageSelector = 'html') {
-    super(page, page.locator('html'))
-    this._waitPageSelector = waitPageSelector
-    this._url = url
-    this.header = new Header(page)
+  constructor(page, url, waitPageSelector = "html") {
+    this._page = page;
+    this._waitPageSelector = waitPageSelector;
+    this._url = url;
+    this.header = new Header(page);
   }
 
   get page() {
-    return this._page
+    return this._page;
   }
 
-  async visit(){
-    await this._page.goto(this._url)
-    await this.waitLoaded()
+  async visit() {
+    await this._page.goto(this._url);
+    await this.waitLoaded();
   }
 
-  async waitLoaded(){
-    await this._page.locator(this._waitPageSelector).waitFor()
+  async waitLoaded() {
+    await this._page.locator(this._waitPageSelector).waitFor();
   }
 }
--- src/pageObjects/welcomePage/WelcomePage.js
+++ src/pageObjects/welcomePage/WelcomePage.js
@@ -5,9 +5,20 @@ import GaragePage from "../garagePage/GaragePage.js"
 
 export default class WelcomePage extends BasePage{
     constructor(page) {
-        super(page, '/', '.header_signin')
+        super(page, '/')
+        this.signUpButton = page.locator('.btn-primary')
+        this.signInButton = page.locator('.header_signin')
+        this.guestLoginButton = page.locator('button.-guest')
+        this.submitButton = page.locator('.btn-primary')
+
     }
 
+    async clickSignUpButtonAndOpenPopUP(){
+
+        await this.signUpButton.click()
+        const popUp = new SignUpPopUp(this._page)
+        return popUp
+    }
     async clickSignInButtonAndOpenPopup(){
         await this.header.signInButton.click()
         const popup = new SignInPopup(this._page)
@@ -18,4 +29,5 @@ export default class WelcomePage extends BasePage{
         await this.header.guestLoginButton.click()
         return new GaragePage(this._page)
     }
+
 }
