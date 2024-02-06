7a0fb159bb10c1d2056b08ccf9a1fd1c78f4102e
Myloslava <88142576+MyloslavaM@users.noreply.github.com>
2024-02-06 01:02:56 +0100

    hw actions

--- src/pageObjects/BasePage.js
+++ src/pageObjects/BasePage.js
@@ -1,7 +1,7 @@
 import Header from "../components/Header.js";
 
 export default class BasePage {
-  constructor(page, url = "https://qauto.forstudy.space/", waitPageSelector = "html") {
+  constructor(page, url, waitPageSelector = "html") {
     this._page = page;
     this._waitPageSelector = waitPageSelector;
     this._url = url;
@@ -13,8 +13,7 @@ export default class BasePage {
   }
 
   async visit() {
-    const targetURL = new URL(this._url);
-    await this._page.goto(targetURL.href); 
+    await this._page.goto(targetURL.href);
     await this.waitLoaded();
   }
 
