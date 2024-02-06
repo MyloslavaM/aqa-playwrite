cab8991de66be26d415fa5fac2e4a7a4c9d2f0a4
Myloslava <88142576+MyloslavaM@users.noreply.github.com>
2024-02-06 02:00:02 +0100

    hw test

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import BaseComponent from "./BaseComponent.js";
 
 export default class Header  extends  BaseComponent{
     constructor(page) {
-        super(page, page.locator('.header'))
-        this.signInButton = this.container.locator('.header_signin')        
+        super(page, page.locator('.container'))
+        this.signInButton = this.container.locator('.btn .btn-outline-white .header_signin')
         this.guestLoginButton = this.container.locator('button.-guest')
     }
 }
